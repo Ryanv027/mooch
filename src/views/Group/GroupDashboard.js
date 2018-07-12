8d6ebd60f75cf62718dfe23a2373b4157b61a760
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-07-11 21:23:25 -0700

    calculation debt and credit

--- src/views/Group/GroupDashboard.js
+++ src/views/Group/GroupDashboard.js
@@ -19,7 +19,37 @@ class GroupDashboard extends React.Component {
       })
       .then(response => {
         this.setState({ expenses: response.data });
+        this.calculateUserBalances(response.data);
       });
+  };
+
+  calculateUserBalances = expenses => {
+    this.props.groupUserData.map(user => {
+      this.getUserBalance(user.userID, expenses);
+    });
+  };
+
+  getUserBalance = (userID, expenses) => {
+    console.log(userID, "AND", expenses);
+    const credits = this.getMyCredits(userID, expenses);
+    const debits = this.getMyDebits(userID, expenses);
+    console.log(credits);
+    console.log(debits);
+  };
+
+  getMyDebits = (userID, expenses) => {
+    return expenses
+      .filter(expense => expense.mooches.includes(userID))
+      .reduce(
+        (acc, cv) => acc + parseInt(cv.amount, 10) / (cv.mooches.length + 1),
+        0
+      );
+  };
+
+  getMyCredits = (userID, expenses) => {
+    return expenses
+      .filter(expense => expense.shark === userID)
+      .reduce((acc, cv) => acc + parseInt(cv.amount, 10), 0);
   };
 
   render() {
